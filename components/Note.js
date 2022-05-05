a77380fee91ce678eab04008fc189135092c8fd2
phuocleoceo <ht10082001@gmail.com>
2022-05-05 13:36:49 +0700

    Note Style

--- components/Note.js
+++ components/Note.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native';
 import useNoteSQLite from "../hooks/useNoteSQLite";
-import { Button } from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
 import React from "react";
 
 export default function Note({ id, title, description, timestamp })
@@ -15,10 +15,20 @@ export default function Note({ id, title, description, timestamp })
 
     return (
         <View style={styles.note}>
-            <Text>{title}</Text>
-            <Text>{description}</Text>
-            <Text>{timestamp}</Text>
-            <Button onPress={handleDeleteNote}>X</Button>
+            <View style={styles.infor}>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.description}>{description}</Text>
+                <Text style={styles.timestamp}>{timestamp}</Text>
+            </View>
+
+            <View style={styles.buttonBar}>
+                <IconButton
+                    icon="delete-forever"
+                    size={27}
+                    onPress={handleDeleteNote}
+                    style={styles.deleteButton}
+                />
+            </View>
         </View>
     );
 }
@@ -26,6 +36,34 @@ export default function Note({ id, title, description, timestamp })
 
 const styles = StyleSheet.create({
     note: {
+        margin: 15,
+        marginBottom: 0,
+        padding: 12,
+        flexDirection: 'row',
+        backgroundColor: "#dce5ea",
+        borderTopRightRadius: 30,
+        borderBottomLeftRadius: 30
+    },
 
+    infor: {
+        flex: 5
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 15,
+        marginBottom: 3
+    },
+    description: {
+        fontSize: 17
+    },
+    timestamp: {
+        fontSize: 13
+    },
+
+    buttonBar: {
+        flex: 1
+    },
+    deleteButton: {
+        alignSelf: "center"
     }
 });
